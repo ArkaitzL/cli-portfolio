17045b59269a6215e3595f83be33716353f94f88
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2025-01-10 15:06:59 +0100

    Basico

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,6 @@ export default {
 	  extend: {
 		colors: {
 		  background: '#1c1917',
-		  card: '#292524',
 		  text: {
 			primary: '#e7e5e4',
 			secondary: '#d6d3d1',
@@ -14,19 +13,9 @@ export default {
 			primary: '#fb923c',
 			secondary: '#fdba74',
 		  },
-		  purple: {
-			300: '#d8b4fe',  // Asumiendo un tono similar al texto "Astro"
-		  },
-		  teal: {
-			300: '#5eead4',  // Asumiendo un tono similar al texto "TailwindCSS"
-		  },
-		  code: {
-			background: '#332e2a',
-			border: '#2b2623',
-		  },
-		  footer: '#4b5563',  // Equivalente a text-gray-600
 		},
 	  },
 	},
 	plugins: [],
-  }
+  };
+  

2fffa6a95ff494b4804a025fc9e54b99aad04317
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2025-01-10 23:13:17 +0100

    -

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -2,6 +2,7 @@
         { id: "perfil", name: "Perfil" },
         { id: "sobremi", name: "SobreMi" },
         { id: "baboongames", name: "BaboonGames" },
+        { id: "tunuevaweb", name: "TuNuevaWeb" },
     ];
     export const error = "error";
     export const help = { id: "help", name: "Help" };
